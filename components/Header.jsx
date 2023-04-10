65d7c40c699ac10a1e26e4cb86e55840a50b375a
JasubiPL <gameportal7@hotmail.com>
2023-04-10 08:24:42 -0600

    add taildwind

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,15 @@
-import { Text } from "@nextui-org/react";
+import { Container, Text } from "@nextui-org/react";
 import Link from "next/link";
 
-export default function Header(){
+export function Header(){
   return(
-    <header>
-      <div>
-        <Text small>next xkcd</Text>
-      </div>
+    <header className="flex justify-between items-center p-4 mx-w-xl m-auto">
+      <h1 className="font-bold">NEXT <span className=" font-light">xkcd</span></h1>
       <nav>
-        <ul>
+        <ul className="flex flex-row gap-2">
           <li><Link href='/'>Home</Link></li>
+          <li><Link href='/about'>About</Link></li>
+          <li><Link href='/search'>Search</Link></li>
         </ul>
       </nav>
     </header>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Container, Card, Row, Text } from "@nextui-org/react";
+import { Header } from '@/components/Header';
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header />
+
       <main>
       <Container>
         <Card css={{ $$cardColor: '$colors$primary' }}>
